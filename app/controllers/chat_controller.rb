6ee550e0bccf78abeb5cba2c6eb37b94adc24bc5
denJunior <denis3getmanov@gmail.com>
2022-06-15 22:38:51 +0300

    init commit

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -1,5 +1,6 @@
 class ChatController < ApplicationController
   def chat
     @users = User.all
+    @user_id = current_user.id
   end
 end
--- app/controllers/user_chat_controller.rb
+++ app/controllers/user_chat_controller.rb
@@ -4,17 +4,22 @@ class UserChatController < ApplicationController
     @message = current_user.sent.create(recipient: recipient, body: message_params[:body])
     @message.save
 
+
+
     redirect_to user_chat_path(user_id: recipient.id)
   end
 
   def user_chat
     @user = User.find params[:user_id]
-    @my_messages = current_user.sent.where(recipient_id: @user.id)
-    @your_messages = current_user.received.where(sender_id: @user.id)
+    @messages = Message.where(recipient_id: @user.id, sender_id: current_user.id)
+                       .or(Message.where(sender_id: @user.id, recipient_id: current_user.id))
+    @old_time = Message.last.created_at
   end
 
-  def save_messages
-    render json: user_chat
+  def messages
+    @user = User.find params[:user_id]
+    @messages_after_update = Message.where(recipient_id: @user.id, sender_id: current_user.id)
+    render json: @messages_after_update
   end
 
   private
