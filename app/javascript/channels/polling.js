494b424be009272a768243aa53f91474e6c68732
den <den@den.com>
2022-06-16 17:27:09 +0300

    websocket

--- app/javascript/channels/polling.js
+++ app/javascript/channels/polling.js
@@ -1,30 +1,30 @@
-// function empty_form(){
-//     let input = document.getElementById('input').value;
-//     if(input === ''){
-//         alert('You forgot to enter text.');
-//     }
-// }
+function empty_form(){
+    let input = document.getElementById('input').value;
+    if(input === ''){
+        alert('You forgot to enter text.');
+    }
+}
 
 
 
-document.addEventListener("DOMContentLoaded", (event) => {
-    const data_id = document.querySelector(".input");
-    let data = data_id.dataset.id;
-    const url = 'user_chat?user_id=' + data
-
-    // переделать на async await
-    // interval
-    //   fetch->GET->server (messages)
-    //  update DOM if there are new messages
-    setInterval(async () => {
-        const res = await fetch(url, {
-            method: 'GET',
-            credentials: 'same-origin',
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
-         const sss = await res.json()
-         console.log(sss)
-    }, 2000);
-});
+// document.addEventListener("DOMContentLoaded", (event) => {
+//     const data_id = document.querySelector(".input");
+//     let data = data_id.dataset.id;
+//     const url = 'user_chat?user_id=' + data
+//
+//     // переделать на async await
+//     // interval
+//     //   fetch->GET->server (messages)
+//     //  update DOM if there are new messages
+//     setInterval(async () => {
+//         const res = await fetch(url, {
+//             method: 'GET',
+//             credentials: 'same-origin',
+//             headers: {
+//                 'Content-Type': 'application/json'
+//             }
+//         });
+//          const sss = await res.json()
+//          console.log(sss)
+//     }, 2000);
+// });
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,8 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import '../channels/polling'
+import '../channels/consumer'
+import '../channels/my_channel'
 
 Rails.start()
 Turbolinks.start()
