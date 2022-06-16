5954d669c821765f90c749f19b96828339a38e60
den <den@den.com>
2022-06-16 11:15:47 +0300

    removed idea, fixed devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,7 @@ class User < ApplicationRecord
   has_many :sent, class_name: 'Message', foreign_key: :sender_id
   has_many :received, class_name: 'Message', foreign_key: :recipient_id
 
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :confirmable, :validatable
+  devise :database_authenticatable, :registerable
 
   validates :email, presence: true, length: { minimum: 5 }
 end
