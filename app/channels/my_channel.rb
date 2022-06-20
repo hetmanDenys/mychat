a36aba2b8c3fc1777ffc823706105038e5a70488
vova <vova4kin@gmail.com>
2022-06-20 13:02:47 +0300

    updates

--- app/channels/my_channel.rb
+++ app/channels/my_channel.rb
@@ -1,7 +1,8 @@
 class MyChannel < Channel
   def subscribed
     # stream_from "user_id=#{params[:user_id]}"
-    stream_from "1"
+    # stream_from "1"
+    stream_from "my"
   end
   def received(data)
     ActionCable.server.broadcast("1", data)
