97a2c779c5a775411146dcfb312a2c378b7efffd
den <den@den.com>
2022-06-22 16:24:07 +0300

    jwt start

--- app/controllers/chat_controller.rb
+++ app/controllers/chat_controller.rb
@@ -1,6 +1,13 @@
 class ChatController < ApplicationController
+
+  before_action :authorized, only: [:auto_login]
+
   def chat
     @users = User.all
     @user_id = current_user.id
+  end
+
+  def auto_login
+    render json: @user
   end
 end
