022f41562cdf114eb97456a5a477c5eb92c6caaa
den <den@den.com>
2022-06-23 17:02:45 +0300

    jwt second day

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   has_many :received, class_name: 'Message', foreign_key: :recipient_id
 
   devise :database_authenticatable, :registerable
+  self.skip_session_storage = [:http_auth, :params_auth]
 
   validates :email, presence: true, length: { minimum: 5 }
 end
