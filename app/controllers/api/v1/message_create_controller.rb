9867070d31a759e05ae4691ef350760541343094
den <den@den.com>
2022-06-24 14:42:16 +0300

    jwt done

--- app/controllers/api/v1/message_create_controller.rb
+++ app/controllers/api/v1/message_create_controller.rb
@@ -1,13 +1,18 @@
-class Api::V1::MessageCreateController < ApplicationController
+class Api::V1::MessageCreateController < Api::V1::ApplicationController
   before_action :authorized
 
   def create
-    #crete messages
+    recipient = User.find params[:user_id]
+    current_user = User.find params[:current_user_id]
+    @message = current_user.sent.create(recipient: recipient, body: params[:body])
+    @message.save
+    @old_time = I18n.l(Message.last.created_at, format: :short)
+    ActionCable.server.broadcast("MyChannel", { body: params[:body], current_user_id: current_user.id, created_at: @old_time })
   end
 
   private
 
   def user_params
-    params.permit(:body)
+    params.permit(:body, :user_id, :current_user_id)
   end
 end
