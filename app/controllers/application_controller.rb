b0825ee593d0c10aa8cc4478f89c000cc07c98f7
denJunior <denis3getmanov@gmail.com>
2022-07-11 17:40:57 +0300

    localization and dotenv

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
   # before_action :extract_locale_from_tld
   before_action :authenticate_user!
+  around_action :switch_locale
+
+  def switch_locale(&action)
+    locale = params[:locale] || I18n.default_locale
+    I18n.with_locale(locale, &action)
+  end
+
+  def localization
+    pp params
+  end
 end
